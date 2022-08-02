9afc9d25a59b977199dadd1c6332213896f61d19
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2022-08-02 15:19:27 +0800

    Updated collapseToggle with Plus Icon modes

--- src/component/sidebar/CollapseToggle.js
+++ src/component/sidebar/CollapseToggle.js
@@ -1,39 +1,51 @@
-import React from 'react';
-import { Form } from 'react-bootstrap'
-import { PlusLg } from 'react-bootstrap-icons';
+import React from "react";
+import { Form } from "react-bootstrap";
+import { PlusLg } from "react-bootstrap-icons";
 
-import '../customStyles.scss';
+import "../customStyles.scss";
 
-const CollapseToggle = ({ handleCollapseToggle, showCollapse, type, label, length, categories }) => {
-  
-  const disabledCategory = (categories.length === 2 
-    && categories.some((value) => { return value.categoryName === type })) || 
+const CollapseToggle = ({
+  handleCollapseToggle,
+  showCollapse,
+  type,
+  label,
+  length,
+  categories,
+}) => {
+  const disabledCategory =
+    (categories.length === 2 &&
+      categories.some((value) => {
+        return value.categoryName === type;
+      })) ||
     categories.length < 2;
 
-  return(
+  return (
     <div className="d-flex justify-content-between align-items-center">
-      <div className="d-flex align-items-center flex-grow-1"
-        onClick={ () => handleCollapseToggle(type) } >
+      <div
+        className="d-flex align-items-center flex-grow-1"
+        onClick={() => handleCollapseToggle(type)}
+      >
         <Form.Check>
-          <Form.Check.Input 
-            id={ label }
+          <Form.Check.Input
+            id={label}
             type="checkbox"
-            checked={ showCollapse }
-            disabled={ disabledCategory ? false : true }
-            onChange={ () => handleCollapseToggle(type) } />
-          <Form.Check.Label 
-            className={ showCollapse ? `text-blue-3`: `` }>
-            { label }
+            checked={showCollapse}
+            disabled={disabledCategory ? false : true}
+            onChange={() => handleCollapseToggle(type)}
+          />
+          <Form.Check.Label className={showCollapse ? `text-blue-3` : ``}>
+            {label}
           </Form.Check.Label>
         </Form.Check>
       </div>
-      { 
-        (showCollapse && length < 2) && 
-        <PlusLg className="icon-style-1" />
-      }
+      {showCollapse && (
+        <PlusLg
+          className="icon-style-1"
+          color={`${length < 2 ? "#636E72" : "#B2BEC3"}`}
+        />
+      )}
     </div>
   );
-    
 };
 
 export default CollapseToggle;
