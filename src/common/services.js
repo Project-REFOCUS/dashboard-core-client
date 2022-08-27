c01c9d13db822033d3fe8ed0fbaa85939502aa39
CThaw90 <christhaw90@gmail.com>
2022-08-27 13:02:08 -0400

    Using state abbreviation name as selection value

--- src/common/services.js
+++ src/common/services.js
@@ -3,7 +3,7 @@ const handleJsonResponse = response => response.json();
 export const getListOfStates = () =>
     window.fetch('/dashboard-service/geography/states')
         .then(handleJsonResponse)
-        .then(results => results.map(state => ({ label: state.name, value: state.id })));
+        .then(results => results.map(state => ({ label: state.name, value: state.shortName })));
 
 export const getRaceEthnicityCategories = () =>
     window.fetch('/dashboard-service/ethnicity')
