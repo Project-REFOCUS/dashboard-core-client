2e547e2ad19948bcd913041e13c73f5f82f3219a
Chris Thaw <chris_thaw@yahoo.com>
2022-09-20 22:49:50 -0400

    Updating data orientation options for Deaths category. Removing percent changes in mortality rate

--- src/common/options.js
+++ src/common/options.js
@@ -19,9 +19,11 @@ export const getDataOrientationOptionsForCases = () =>
 export const getDataOrientationOptionsForDeaths = () =>
     getBaseDataOrientationOptions('deaths').concat([
         { label: 'Mortality rate (death rate)', value: 'mortalityRate', order: 31 },
-        { label: 'Percent change in mortality rate', value: 'percentChangeInMortalityRate', order: 32 },
-        { label: 'Percent change in mortality rate over 7 days', value: 'percentChangeInMortalityRateOver7', order: 33 },
-        { label: 'Percent change in mortality rate over 14 days', value: 'percentChangeInMortalityRateOver14', order: 34 }
+        { label: 'Mortality rate over 7 days', value: 'mortalityRateOver7Days', order: 32 },
+        { label: 'Mortality rate over 14 days', value: 'mortalityRateOver14Days', order: 33 },
+        // { label: 'Percent change in mortality rate', value: 'percentChangeInMortalityRate', order: 34 },
+        // { label: 'Percent change in mortality rate over 7 days', value: 'percentChangeInMortalityRateOver7', order: 35 },
+        // { label: 'Percent change in mortality rate over 14 days', value: 'percentChangeInMortalityRateOver14', order: 36 }
     ]).sort(sortOrder).map(mappedOrientation);
 
 export const getDataOrientationOptionsForTests = () =>
